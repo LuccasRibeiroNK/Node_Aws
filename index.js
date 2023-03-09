86911919932f07a517d274424dc67d5ebc1191e7
LuccasRibeiroNK <luckgl@hotmail.com>
2023-03-09 01:43:18 -0300

    quarto envio

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-   return response.send({message: 'server is running'});
-    });
+    return res.send('Server is running');
+});
 
 app.post('/teste', (req, res) => {
     const {name, date} = req.body;
     return res.json({name, date});
 });
 
+
+
 app.listen(3333);
+
+// app.get('/', (req, res) => {
+//    return res.send({message: 'server is running'});
+//     });
+
+// app.post('/teste', (req, res) => {
+//     const {name, date} = req.body;
+//     return res.json({name, date});
+// });
+
+// app.listen(3333);
