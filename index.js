15ecd211052f67f39bf86b4da687e309c6896f95
LuccasRibeiroNK <luckgl@hotmail.com>
2023-03-09 01:21:43 -0300

    segundo envio

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-   return response.send(message: 'server is running');
+   return response.send({message: 'server is running'});
     });
+
+app.post('/teste', (req, res) => {
+    const {name, date} = req.body;
+    return response.json({name, date});
+});
 
 app.listen(3333);
