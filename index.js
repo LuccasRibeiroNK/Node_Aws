8daa566a861c1ec933177681ceda7ac99310ddb4
LuccasRibeiroNK <luckgl@hotmail.com>
2023-03-09 16:33:28 -0300

    add mysql connection

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.send({message:'Server is running on port 3333'});
+const db = mysql.createConnection({
+    host: "mysqlserver.cntsqjxnav68.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "admin123",
+    database: "test"
 });
 
-app.get('/atualizar', (req, res) => {
-    return res.send({message:'Atualizou mesmo para o nodemon!'});
-});
-
-app.post('/teste', (req, res) => {
-    const {name, date} = req.body;
-    return res.json({name, date});
-});
-
-app.listen(3333, () => {
-    console.log('Server is running on port 3333');
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the application." });
 });
 
 // app.get('/', (req, res) => {
-//    return res.send({message: 'server is running'});
-//     });
+//     return res.send({message:'Server is running on port 3333'});
+// });
+
+// app.get('/atualizar', (req, res) => {
+//     return res.send({message:'Atualizou mesmo para o nodemon!'});
+// });
 
 // app.post('/teste', (req, res) => {
 //     const {name, date} = req.body;
 //     return res.json({name, date});
 // });
 
-// app.listen(3333);
+app.listen(3333, () => {
+    console.log('Server is running on port 3333');
+});
+
