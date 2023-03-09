7c69dccf494a0416c3e57b91a79aa5f9721fe434
LuccasRibeiroNK <luckgl@hotmail.com>
2023-03-09 19:48:06 -0300

    2final fix

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 import express from 'express';
-import { createConnection } from 'mysql2';
+// import { createConnection } from 'mysql2';
 
 const app = express();
 
 // app.use(express.json());
 
-const db = createConnection({
-    host: "mysqlserver.cntsqjxnav68.us-east-1.rds.amazonaws.com",
-    user: "admin",
-    password: "admin123",
-    database: "test"
-});
+// const db = createConnection({
+//     host: "mysqlserver.cntsqjxnav68.us-east-1.rds.amazonaws.com",
+//     user: "admin",
+//     password: "admin123",
+//     database: "test"
+// });
 
-db.query('SELECT * FROM books', (err, result) => {
-    if (err) {  console.log(err); }
-    else {  console.log(result); }
-});
+// db.query('SELECT * FROM books', (err, result) => {
+//     if (err) {  console.log(err); }
+//     else {  console.log(result); }
+// });
 
 
-app.get('/books', (req, res) => {
-    return res.send({message:'Books!'});
+app.get('/', (req, res) => {
+    return res.send({message:'Hello World!'});
 });
 
 // app.get('/atualizar', (req, res) => {
