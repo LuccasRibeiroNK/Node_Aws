970daa8b6360d89b33132d7896dfd480c1e9bf50
LuccasRibeiroNK <luckgl@hotmail.com>
2023-03-09 16:15:12 -0300

    teste de novo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    return res.send({message:'Server is running'});
+    return res.send({message:'Server is running on port 3333'});
 });
 
 app.get('/atualizar', (req, res) => {
