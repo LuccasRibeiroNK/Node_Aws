7e8e0f6aafc6f7c80fe90e8d0b4087b07f65db13
LuccasRibeiroNK <luckgl@hotmail.com>
2023-04-03 14:38:30 -0300

    adicionando price

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const mysql = require('mysql2');
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-
 
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
@@ -22,51 +21,47 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to database:', err);
+    console.error("Error connecting to database:", err);
     return;
   }
-  console.log('Connected to database!');
+  console.log("Connected to database!");
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!'
-    // create a button to page: produtos
-    + '<br><br><a href="/produtos">produtos</a>'
-
-    );
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/livros', (req, res) => {
-  connection.query('SELECT * FROM livros', (err, results) => {
+app.get("/livros", (req, res) => {
+  connection.query("SELECT * FROM livros", (err, results) => {
     if (err) {
-      console.error('Error querying database:', err);
-      res.status(500).send('Error querying database');
+      console.error("Error querying database:", err);
+      res.status(500).send("Error querying database");
       return;
     }
     res.json(results);
   });
 });
 
-app.post('/livros', (req, res) => {
-    const q = 'INSERT INTO livros (`title`, `desc`, `cover`) VALUES (?)';
-    const values = [
-        req.body.title,
-        req.body.desc,
-        req.body.cover
-    ];
+app.post("/livros", (req, res) => {
+  const q =
+    "INSERT INTO livros (`title`, `desc`, `price`, `cover` ) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
 
-    connection.query(q, [values], (err, results) => {
-        if (err) {
-            console.error('Error querying database:', err);
-            res.status(500).send('Error querying database');
-            return;
-        }
-        res.status(201).send('Livro criado com sucesso!');
-    });
+  connection.query(q, [values], (err, results) => {
+    if (err) {
+      console.error("Error querying database:", err);
+      res.status(500).send("Error querying database");
+      return;
+    }
+    res.status(201).send("Livro criado com sucesso!");
+  });
 });
-
 
 app.listen(3333, () => {
-    console.log('Server is running on port: 3333');
+  console.log("Server is running on port: 3333");
 });
-
